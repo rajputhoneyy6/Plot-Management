c64723e23c088a76d261828a66316402029e0a58
rajputhoneyy6 <rajput.honeyy6@gmail.com>
2026-02-23 15:26:15 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -14,10 +14,7 @@ function Register() {
 
   const handleRegister = async () => {
     try {
-      await axios.post(
-        "http://localhost:4090/api/auth/register",
-        form
-      );
+      await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/register`, data,form);
 
       alert("Registered Successfully");
       navigate("/login");
