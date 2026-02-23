a3cbf7bb674e1abf7840880ae3fd9ee16b00861b
rajputhoneyy6 <rajput.honeyy6@gmail.com>
2026-02-23 15:27:42 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -14,10 +14,7 @@ function Login() {
   const handleLogin = async () => {
     try {
 
-      const res = await axios.post(
-        "http://localhost:4090/api/auth/login",
-        { email, password }
-      );
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/register`,{ email, password });
 
       login(res.data);
 
